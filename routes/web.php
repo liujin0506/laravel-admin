4e20e282e447ec8cedd7221dbff7e4edc71c7118
liujing <liujin0506@qq.com>
2018-08-02 09:16:47 +0800

    init project

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+$api = app('Dingo\Api\Routing\Router');
+$api->version('v1', function ($api) {
+   $api->get('/', function() {
+      return ['11', '22'];
+  });
+});
