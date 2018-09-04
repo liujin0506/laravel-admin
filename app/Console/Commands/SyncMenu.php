d3dec1660e355d95bde192d41103ed9a2c8e99ab
刘静 <liujing@cmstop.com>
2018-09-04 19:08:52 +0800

    + 自定义菜单

---
+++ app/Console/Commands/SyncMenu.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Description  CmsTop MediaCloud
+ *
+ * @Author      liujing <liujing@cmstop.com>
+ * @DateTime    2018/9/4 19:04
+ * @CopyRight   Beijing CmsTop Technology Co.,Ltd.
+ */
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SyncMenu extends Command
+{
+    protected $signature = 'sync:menu';
+    protected $description = '同步微信菜单';
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * 命令执行
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $app = app('wechat.official_account');
+        $buttons = [
+            [
+                "type" => "view",
+                "name" => "京好赚",
+                "url"  => "http://wx.jhz.bjue.cn/"
+            ]
+        ];
+        $app->menu->create($buttons);
+        return 'success';
+    }
+}
